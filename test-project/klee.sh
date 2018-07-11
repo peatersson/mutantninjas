463974e675f8a1d7acdc6aa4d13dd5cb52e44d13
Niklas <nikpe353@student.liu.se>
2018-07-11 17:31:56 -0400

    fix(klee-result): reporting result from every KLEE execution.

--- test-project/klee.sh
+++ test-project/klee.sh
@@ -7,21 +7,47 @@ for file_to_compile in *_klee_*;
 do
     clang -I $pwd/klee_src/include/ -emit-llvm -c -g $file_to_compile
     file_to_run=$(ls | grep ".bc")
-    klee $file_to_run
+    klee -max-time=10 $file_to_run
 
-    klee_fail_run=$(ls klee-last | grep ".err")
-    klee_fail_run=${klee_fail_run%%.*}
+    cd klee-out-0
+    klee_fail_run=$(ls | grep ".err")
 
-    if [ $klee_fail_run ]
+    if [[ $klee_fail_run = *".abort."* ]]
     then
-        cd klee-last
+        klee_fail_run=${klee_fail_run%%.*}
         klee_fail_out=$(ktest-tool --write-ints $klee_fail_run.ktest)
-        echo $klee_fail_out >> ../../errors.txt
-        cd ..
+
+        echo "Abort:"$klee_fail_out >> ../../result.txt
+
+    elif [[ $klee_fail_run = *".assert."* ]]
+    then
+        klee_fail_run=${klee_fail_run%%.*}
+        klee_fail_out=$(ktest-tool --write-ints $klee_fail_run.ktest)
+
+        echo "Assert:"$klee_fail_out >> ../../result.txt
+    else
+        klee_fail_run=$(ls | grep ".early")
+
+        if [ $klee_fail_run ]
+        then
+            echo "Halt:"$file_to_compile >> ../../result.txt
+        else
+            echo "Eq:"$file_to_compile >> ../../result.txt
+        fi
+
     fi
 
+    cd ..
     rm $file_to_run
 done
 
 # remove the previously created temporary directory
 rm -rf klee-out-* klee-last
+
+
+
+
+#if [[ $klee_fail_run = *".early" ]]
+#then
+    #echo "Halt:$file_to_compile" >> ../../errors.txt
+#el
